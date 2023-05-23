06adae8c62f8add419bc84c3c12e56f114380b7f
k5va <k5va@yandex.ru>
2023-05-23 12:36:18 +0500

    Extracts auth options to lib

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,17 +1,6 @@
-import { prisma } from '@/db';
 import NextAuth from 'next-auth';
-import { PrismaAdapter } from '@next-auth/prisma-adapter';
-import GithubProvider from 'next-auth/providers/github';
-import { env } from '@/env.mjs';
+import { authOptions } from '@/lib/auth/auth-options';
 
-const handler = NextAuth({
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    GithubProvider({
-      clientId: env.GITHUB_CLIENT_ID,
-      clientSecret: env.GITHUB_CLIENT_SECRET,
-    }),
-  ],
-});
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
